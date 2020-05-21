7c8ac7111a05584c5fa0cbef7ae7c817368bf83d
Victor Fernandez <vmfs99@gmail.com>
2020-05-21 02:15:58 -0400

    done no picture of me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,12 +20,26 @@ export default class About extends Component {
       <section className="about">
         <div className="about-story">
           <h2 className="about-story-title">Background</h2>
+          <p className="about-story-paragraph">
+            Self taugh web developer since June 2019. Attended Ironhack Miami
+            Coding Bootcamp from January 2020 to March 2020.
+          </p>
         </div>
         <div className="about-picture"></div>
         <div className="about-skills">
           <h2 className="about-skills-title">Skills</h2>
           <div className="about-skills-list">
-            {this.showSkills(["html", "sass"])}
+            {this.showSkills([
+              "html",
+              "sass",
+              "javascript",
+              "reactJS",
+              "redux",
+              "mongoDB",
+              "firebase",
+              "nodeJS",
+              "expressJS",
+            ])}
           </div>
         </div>
       </section>
