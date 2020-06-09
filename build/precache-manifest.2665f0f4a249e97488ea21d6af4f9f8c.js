da2d619941a5bd20995b0115e5884a0e75814e48
Victor Fernandez <vmfs99@gmail.com>
2020-06-08 20:39:56 -0400

    optimize

---
+++ build/precache-manifest.2665f0f4a249e97488ea21d6af4f9f8c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71103c3841219a584981c9bc750663a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "951b6746739eee52a11e",
+    "url": "/static/css/main.be87702e.chunk.css"
+  },
+  {
+    "revision": "19c91a59bb17b7d974c8",
+    "url": "/static/js/2.33691426.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.33691426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951b6746739eee52a11e",
+    "url": "/static/js/main.379afeb7.chunk.js"
+  },
+  {
+    "revision": "286249a9c887aa7bb957",
+    "url": "/static/js/runtime-main.d41366b1.js"
+  },
+  {
+    "revision": "1c8b60174002e8578b185fd123bd21f5",
+    "url": "/static/media/ModernPlate.1c8b6017.ttf"
+  },
+  {
+    "revision": "44b223ad896c78b16682dcf37a6bb6af",
+    "url": "/static/media/archia-regular-webfont.44b223ad.ttf"
+  },
+  {
+    "revision": "7a3b54cf91d2b5e234bf30bd7c8aa37b",
+    "url": "/static/media/cheeriologo.7a3b54cf.png"
+  },
+  {
+    "revision": "0d56202fd54e6fdabea5b797e8c04ea1",
+    "url": "/static/media/csslogo.0d56202f.png"
+  },
+  {
+    "revision": "9de32921fe1e3899c721d066d10da3ef",
+    "url": "/static/media/expressjslogo.9de32921.svg"
+  },
+  {
+    "revision": "458da78b93cdc20d7a0d950ada04cf5f",
+    "url": "/static/media/firebase.458da78b.png"
+  },
+  {
+    "revision": "d755ccc4c4c80d27e5b062e5a1c3bfda",
+    "url": "/static/media/fitsocialdemo.d755ccc4.gif"
+  },
+  {
+    "revision": "461852a0e75a7be049b6dc61d526074f",
+    "url": "/static/media/githublogo.461852a0.png"
+  },
+  {
+    "revision": "49f40012d36bb86e6f219a7fd00c5048",
+    "url": "/static/media/htmllogo.49f40012.png"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "b390a0aa45c05dc060e444986b45e25b",
+    "url": "/static/media/laptop-bg-landing.b390a0aa.png"
+  },
+  {
+    "revision": "2b14d5610cecf4026b74bc7d47426b18",
+    "url": "/static/media/livestylebg.2b14d561.png"
+  },
+  {
+    "revision": "97e2b068c5ce3b170e4325b904037623",
+    "url": "/static/media/livestyledemo.97e2b068.gif"
+  },
+  {
+    "revision": "ef76cdb954fad3eb6e081a2d18144249",
+    "url": "/static/media/mongodblogo.ef76cdb9.png"
+  },
+  {
+    "revision": "85458a6717bed07d7db5146928d3cdd0",
+    "url": "/static/media/nodejslogo.85458a67.png"
+  },
+  {
+    "revision": "5a439fda2b20ca5ba74baf6779ecffee",
+    "url": "/static/media/officebg.5a439fda.jpg"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/static/media/reactjslogo.82a318cc.png"
+  },
+  {
+    "revision": "c8f44df3b0bdbe3addb48058ed44c088",
+    "url": "/static/media/reduxlogo.c8f44df3.png"
+  },
+  {
+    "revision": "f978ee4a6bd3c5ef1ec06359d6cd291d",
+    "url": "/static/media/sanspicBareLogo.f978ee4a.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/static/media/sasslogo.b446ca5a.svg"
+  },
+  {
+    "revision": "0c6303ea75a8d0b1342a6339fe3d2b5f",
+    "url": "/static/media/tracktradebg.0c6303ea.png"
+  },
+  {
+    "revision": "2929d8297eac52ed5eef6cc4136e7d00",
+    "url": "/static/media/tracktradedemo.2929d829.gif"
+  }
+]);
