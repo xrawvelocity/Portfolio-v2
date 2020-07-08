3b9014ce776794a5bab47b4e02770abd4b174f60
Victor Fernandez <vmfs99@gmail.com>
2020-07-08 17:20:12 -0400

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
-
 export default class Home extends Component {
   render() {
     return (
